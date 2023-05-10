636e90395ab113b51e1a54f96f5a1abb0280ae30
axelbriandip <axelbriandip.rg@gmail.com>
2023-05-09 21:47:06 -0300

    cbv in home done

--- core/views.py
+++ core/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
 
-def sample(request):
-    return render(request, "core/sample.html")
+    # forma #1: opción larga para definir un diccionario de contexto
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["test"] = "123"
+    #     return context
+
+    # forma #2: opción corta para definir un diccionario de contexto
+    def get(self, request):
+        return render(request, self.template_name, { 'test': 'este es el test' })
+
+class SamplePageView(TemplateView):
+    template_name = 'core/sample.html'
