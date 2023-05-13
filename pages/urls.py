42747f5a0c63df27dce51ef4b7a25ead0502ce04
axelbriandip <axelbriandip.rg@gmail.com>
2023-05-13 18:52:55 -0300

    EditView done

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PageListViews, PageDetailViews, PageCreate
+from .views import PageListViews, PageDetailViews, PageCreate, PageUpdateView
 
 pages_patterns = ([
     path('', PageListViews.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailViews.as_view(), name='page'),
-    path('create/', PageCreate.as_view(), name='create')
+    path('create/', PageCreate.as_view(), name='create'),
+    path('update/<int:pk>', PageUpdateView.as_view(), name='update')
 ], 'pages')
