29f331dd6408ba931c5bd74a5ff4b22d559ab14e
axelbriandip <axelbriandip.rg@gmail.com>
2023-06-02 11:42:07 -0300

    reset password done

--- webplayground_udemy/settings.py
+++ webplayground_udemy/settings.py
@@ -129,3 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Config auth
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+import os
+# Emails
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+else:
+    # Configurar un email real para producción
+    pass
