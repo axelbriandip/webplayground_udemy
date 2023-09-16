14b75807f2a93159f9611b976535ff6c98e9725d
axelbriandip <axelbriandip.rg@gmail.com>
2023-09-16 05:11:55 -0300

    page list view done

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PagesListView
 
 urlpatterns = [
-    path('', views.pages, name='pages'),
+    path('', PagesListView.as_view(), name='pages'),
     path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
 ]
