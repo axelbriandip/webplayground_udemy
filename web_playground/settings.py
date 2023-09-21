1c7c0e2a768be5f7686b798593731413eee43fa7
axelbriandip <axelbriandip.rg@gmail.com>
2023-09-21 10:55:53 -0300

    password reset done

--- web_playground/settings.py
+++ web_playground/settings.py
@@ -115,3 +115,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Auth redirect
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+import os
+
+# Emails
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+else:
+    # Acá hay que configurar email real para producción
+    pass
