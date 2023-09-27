e8cfe0784e47e4093865cdd4d539c8c9b6171b08
axelbriandip <axelbriandip.rg@gmail.com>
2023-09-27 11:27:53 -0300

    form update profile ok

--- registration/views.py
+++ registration/views.py
@@ -1,8 +1,10 @@
-from django.views.generic.edit import CreateView
+from typing import Any
+from django.db import models
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django import forms
 from .forms import CreateUserWithEmail
-from django.views.generic.base import TemplateView
+from .models import Profile
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
@@ -31,5 +33,13 @@ class SignUpView(CreateView):
         return reverse_lazy('login') + '?register'
 
 @method_decorator(login_required, name='dispatch')
-class ProfileUpdate(TemplateView):
+class ProfileUpdate(UpdateView):
+    model = Profile
+    fields = ['avatar', 'bio', 'link']
+    success_url = reverse_lazy('profile')
     template_name = "registration/profile_form.html"
+
+    def get_object(self):
+        # recuperar el objeto que se va a editar
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return profile
