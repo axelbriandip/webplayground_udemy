6287d7f27a21168f3111ef5da7d9d54ac26a8552
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-04-26 18:08:33 -0300

    optimize avatars add

--- registration/models.py
+++ registration/models.py
@@ -3,11 +3,16 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
+def custom_profile_picture_path(instance, filename):
+    old_instance = Profile.objects.get(pk=instance.pk)
+    old_instance.profile_picture.delete()
+    return 'profile_pictures/' + filename
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to=custom_profile_picture_path, blank=True, null=True)
     link = models.URLField(blank=True, null=True)
 
 @receiver(post_save, sender=User)
