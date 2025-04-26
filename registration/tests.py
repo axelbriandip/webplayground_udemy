38b5bc19114376b1d3302fc605ede4e76625bc69
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-04-26 17:57:18 -0300

    test registration add

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        # Create a user instance
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_profile_create(self):
+        exists = Profile.objects.filter(user__username='testuser').exists()
+        self.assertTrue(exists, True)
