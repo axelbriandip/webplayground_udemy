e8fed381ed5595a036ad12c26c974860caf670ef
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-04-26 21:59:41 -0300

    test user is not in thread add message, fail

--- messenger/tests.py
+++ messenger/tests.py
@@ -7,6 +7,7 @@ class ThreadTestCase(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user('user1', None, 'password1')
         self.user2 = User.objects.create_user('user2', None, 'password2')
+        self.user3 = User.objects.create_user('user3', None, 'password3')
         self.thread = Thread.objects.create()
 
     def test_add_users_to_thread(self):
@@ -31,3 +32,11 @@ class ThreadTestCase(TestCase):
 
         for message in self.thread.messages.all():
             print("({}): {}".format(message.user.username, message.content))
+    
+    def test_add_message_from_user_not_in_thread(self):
+        self.thread.users.add(self.user1, self.user2)
+        message1 = Message.objects.create(user=self.user1, content="Hello!")
+        message2 = Message.objects.create(user=self.user2, content="Hello! how are you?")
+        message3 = Message.objects.create(user=self.user3, content="Jojojooo")
+        self.thread.messages.add(message1, message2, message3)
+        self.assertEqual(len(self.thread.messages.all()), 2)
