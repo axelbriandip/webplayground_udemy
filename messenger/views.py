eee90619a813c8e6e23caa4a17ffc803c9f4f987
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-05-01 13:54:49 -0300

    add item messenger in nav

--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from .models import Thread
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.http import Http404
 
 # Create your views here.
+@method_decorator(login_required, name='dispatch')
+class ThreadList(TemplateView):
+    template_name = 'messenger/thread_list.html'
+
+@method_decorator(login_required, name='dispatch')
+class ThreadDetail(DetailView):
+    model = Thread
+
+    # verificar si el request.user es parte de los usuarios del hilo
+    def get_object(self):
+        obj = super(ThreadDetail, self).get_object()
+        if self.request.user not in obj.users.all():
+            raise Http404() # en caso de que no sea parte del hilo
+        return obj # en caso de que si sea parte del hilo, se retorna el objeto
--- webplayground/urls.py
+++ webplayground/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from registration.urls import registration_patterns
+from messenger.urls import messenger_patterns
 from profiles.urls import profiles_patterns
 from django.conf import settings
 
@@ -30,6 +31,8 @@ urlpatterns = [
     path('accounts/', include(registration_patterns)),
     # profiles urls
     path('profiles/', include(profiles_patterns)),
+    # messenger urls
+    path('messenger/', include(messenger_patterns)),
 ]
 
 if settings.DEBUG:
